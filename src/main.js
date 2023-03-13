878d676706b8eee572ba813eaa22b07c70ecaf74
AlexeyShelyagin <42693753+AlexeysShelyagin@users.noreply.github.com>
2023-03-13 20:50:10 +0300

    canvas tests

--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@ import { Game } from "./game.js";
 let game = new Game();
 
 game.game_loop()
+
+let cx = document.getElementById("test").getContext("2d");
+
+let img = new Image();
+img.src = '../img/grass.png';
+img.onload = function (){
+    cx.drawImage(img, 0, 0, 50, 50);
+    cx.drawImage(img, 50, 0, 50, 50);
+};
