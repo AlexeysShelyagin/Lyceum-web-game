16c02ee6940b858362b24568f050e08216696b8e
AlexeyShelyagin <42693753+AlexeysShelyagin@users.noreply.github.com>
2023-03-13 22:29:40 +0300

    renamed img to sprites

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ canvas.height = screen.height;
 console.log(screen.width, canvas.width);
 
 let img = new Image();
-img.src = '../img/grass.png';
+img.src = '../sprites/bg2.png';
 img.onload = function (){
     cx.drawImage(img, 0, 0, canvas.width / 20, canvas.width / 20);
     cx.drawImage(img, 100, 0, canvas.width / 20, canvas.width / 20);
