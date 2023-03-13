6f456a9569664a774d2d061cf70577b0b17d3dab
AlexeyShelyagin <42693753+AlexeysShelyagin@users.noreply.github.com>
2023-03-13 22:25:46 +0300

    canvas field created

--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ let game = new Game();
 
 game.game_loop()
 
-let cx = document.getElementById("test").getContext("2d");
+let screen = document.getElementById("game_screen").getBoundingClientRect();
+let canvas = document.getElementById("test");
+let cx = canvas.getContext("2d");
+canvas.width = screen.width;
+canvas.height = screen.height;
+
+console.log(screen.width, canvas.width);
 
 let img = new Image();
 img.src = '../img/grass.png';
 img.onload = function (){
-    cx.drawImage(img, 0, 0, 50, 50);
-    cx.drawImage(img, 50, 0, 50, 50);
+    cx.drawImage(img, 0, 0, canvas.width / 20, canvas.width / 20);
+    cx.drawImage(img, 100, 0, canvas.width / 20, canvas.width / 20);
 };
